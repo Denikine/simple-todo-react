f5f23940a11bfe140862d5f9874f59fcf8926c7f
Tugdual <40633179+Denikine@users.noreply.github.com>
2022-06-08 22:22:43 +0200

    update

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,21 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Task } from './Task';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { TaskForm } from './TaskForm';
 
+
+
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+const pendingTasksCount = useTracker(() =>
+    TasksCollection.find(hideCompletedFilter).count()
+  );
+const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+  const [hideCompleted, setHideCompleted] = useState(false);
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+  const tasks = useTracker(() => TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 } }).fetch());
 
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>
+              📝️ To Do List {pendingTasksTitle}
+            </h1>
+          </div>
+        </div>
+      </header>
 
-      <TaskForm/>
-
-      <ul>
-        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-      </ul>
+      <div className="main">
+        <TaskForm />
+        <div className="filter">
+          <button onClick={() => setHideCompleted(!hideCompleted)}>
+            {hideCompleted ? 'Show All' : 'Hide Completed'}
+          </button>
+        </div>
+        <ul className="tasks">
+          {tasks.map(task => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
